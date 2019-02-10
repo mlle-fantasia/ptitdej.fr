13f55c6169c0823b2aaaa7bdef7c1581255cce28
marina63 <marinafront@hotmail.fr>
2019-02-10 01:17:27 +0100

    nouveau titre formulaire par default

--- web/js/ptidej.js
+++ web/js/ptidej.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
     $('.detailsDynamique').hide();
-    $('.entreprise').css('margin-top','158px' );
 });
 
 
-$("#inscription_etape1_nature").change(function() {
+$("#inscription_etape1_nature").on('change', function() {
     $(".detailsDynamique").hide();
     var radioValue = $("input[name='inscription_etape1[nature]']:checked").val();
-    if(radioValue){
-        $('.entreprise').css('margin-top','30px' );
-        $("."+radioValue+"Dynamique").show();
-    }
+    $('.entreprise').css('margin-top','30px' );
+    $("."+radioValue+"Dynamique").show();
 
 });
-
 
 
 
